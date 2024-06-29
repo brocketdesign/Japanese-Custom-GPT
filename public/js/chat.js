7e41dd325ab25483667c6ef06569b4a7b2f0db7e
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2024-06-29 14:48:42 +0900

    design design

--- public/js/chat.js
+++ public/js/chat.js
@@ -179,7 +179,7 @@ $(document).ready(function() {
                         <div id="container-${designStep}">
                             <div class="d-flex flex-row justify-content-start mb-4 message-container">
                                 <img src="${thumbnail || 'https://lamix.hatoltd.com/img/logo.webp' }" alt="avatar 1" class="rounded-circle" style="min-width: 45px; width: 45px; height: 45px; border-radius: 15%;object-fit: cover;">
-                                <div id="message-${designStep}" class="p-3 ms-3 text-start" style="border-radius: 15px; background: linear-gradient(90.9deg, rgba(247, 243, 255, 0.5) 2.74%, #B894F9 102.92%);">
+                                <div id="message-${designStep}" class="p-3 ms-3 text-start" style="border-radius: 15px; background: linear-gradient(90.9deg, rgba(247, 243, 255, 0.5) 2.74%, #e8e8e8 102.92%);">
                                     ${marked.parse(assistantMessage.content)}
                                 </div>
                             </div>
@@ -225,7 +225,7 @@ $(document).ready(function() {
             <div id="container-${currentStep}">
                 <div class="d-flex flex-row justify-content-start mb-4 message-container" style="opacity:0;">
                     <img src="${ thumbnail ? thumbnail : 'https://lamix.hatoltd.com/img/logo.webp' }" alt="avatar 1" class="rounded-circle" style="min-width: 45px; width: 45px; height: 45px; border-radius: 15%;object-fit: cover;">
-                    <div id="message-${currentStep}" class="p-3 ms-3 text-start" style="border-radius: 15px;   background: linear-gradient(90.9deg, rgba(247, 243, 255, 0.5) 2.74%, #B894F9 102.92%);"></div>
+                    <div id="message-${currentStep}" class="p-3 ms-3 text-start" style="border-radius: 15px;   background: linear-gradient(90.9deg, rgba(247, 243, 255, 0.5) 2.74%, #e8e8e8 102.92%);"></div>
                 </div>
                 <div id="response-${currentStep}" class="choice-container" ></div>
             </div>`)
@@ -252,7 +252,7 @@ $(document).ready(function() {
                 <div id="container-${currentStep}">
                     <div class="d-flex flex-row justify-content-start mb-4 message-container" style="opacity:0;">
                         <img src="${ thumbnail ? thumbnail : 'https://lamix.hatoltd.com/img/logo.webp' }" alt="avatar 1" class="rounded-circle" style="min-width: 45px; width: 45px; height: 45px; border-radius: 15%;object-fit: cover;">
-                        <div id="message-${currentStep}" class="p-3 ms-3 text-start" style="border-radius: 15px;   background: linear-gradient(90.9deg, rgba(247, 243, 255, 0.5) 2.74%, #B894F9 102.92%);"></div>
+                        <div id="message-${currentStep}" class="p-3 ms-3 text-start" style="border-radius: 15px;   background: linear-gradient(90.9deg, rgba(247, 243, 255, 0.5) 2.74%, #e8e8e8 102.92%);"></div>
                     </div>
                     <div id="response-${currentStep}" class="choice-container" ></div>
                 </div>`)
@@ -356,7 +356,7 @@ $(document).ready(function() {
                         <div id="container-${currentStep}">
                             <div class="d-flex flex-row justify-content-start mb-4 message-container">
                                 <img src="${ thumbnail ? thumbnail : 'https://lamix.hatoltd.com/img/logo.webp' }" alt="avatar 1" class="rounded-circle" style="min-width: 45px; width: 45px; height: 45px; border-radius: 15%;object-fit: cover;">
-                                <div id="completion-${currentStep}" class="p-3 ms-3 text-start" style="border-radius: 15px;   background: linear-gradient(90.9deg, rgba(247, 243, 255, 0.5) 2.74%, #B894F9 102.92%);"></div>
+                                <div id="completion-${currentStep}" class="p-3 ms-3 text-start" style="border-radius: 15px;   background: linear-gradient(90.9deg, rgba(247, 243, 255, 0.5) 2.74%, #e8e8e8 102.92%);"></div>
                             </div>
                             <div id="response-${currentStep}" class="choice-container" ></div>
                         </div>`);

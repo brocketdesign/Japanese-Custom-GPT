b81f4c24a722836562c01c8c34fb793c9422776f
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2024-08-01 15:18:31 +0900

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -557,7 +557,7 @@ $(document).ready(function() {
                     <div class="d-flex flex-row justify-content-start mb-4 message-container">
                         <img src="${ thumbnail ? thumbnail : 'https://lamix.hatoltd.com/img/logo.webp' }" alt="avatar 1" class="rounded-circle" style="min-width: 45px; width: 45px; height: 45px; border-radius: 15%;object-fit: cover;object-position:top;">
                         <div id="completion-${currentStep}" class="p-3 ms-3 text-start" style="border-radius: 15px;">
-                            <img src="/img/load-dot.gif" width="50px">
+                            <img src="https://lamix.hatoltd.com/img/load-dot.gif" width="50px">
                         </div>
                     </div>
                     <div id="response-${currentStep}" class="choice-container" ></div>

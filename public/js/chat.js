489e8953c789007e1f5918f5d1650facfa451215
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2024-08-10 19:15:31 +0900

    add image background

--- public/js/chat.js
+++ public/js/chat.js
@@ -249,7 +249,7 @@ $(document).ready(function() {
 
                         chatName = data.chat.name
                         thumbnail = data.chat.thumbnailUrl || data.chat.chatImageUrl
-
+                        $('#chat-container').css(`background-image`,`url(${thumbnail})`)
                         $('#chat-title').text(chatName)
                         $('#input-container').show().addClass('d-flex');
 

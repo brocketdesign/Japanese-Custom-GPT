820611e57ef4867814a402b66830f2243c74f959
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2024-08-14 19:41:46 +0900

    add character debugging

--- models/tool.js
+++ models/tool.js
@@ -1,3 +1,14 @@
+
+const axios = require('axios');
+const { createHash } = require('crypto');
+const aws = require('aws-sdk');
+
+// Configure AWS S3
+const s3 = new aws.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
 // Function to get the current counter value from the database
 async function getCounter(db) {
   const counterDoc = await db.collection('counters').findOne({ _id: 'storyCounter' });
--- routes/api.js
+++ routes/api.js
@@ -11,12 +11,6 @@ const axios = require('axios');
 
 async function routes(fastify, options) {
 
-    // Configure AWS S3
-    const s3 = new aws.S3({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION
-    });
 
     fastify.get('/api/urlsummary', async (request, reply) => {
         const url = request.query.url;

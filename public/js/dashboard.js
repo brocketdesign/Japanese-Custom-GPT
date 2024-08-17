80b0b104426776c679b734cdca20b44fe133476a
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2024-08-17 23:33:36 +0900

    Update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -213,7 +213,7 @@ window.showCoinShop = function(){
                     <div class="col-6 my-3">
                         <div class="card bg-dark text-white">
                             <div class="card-body p-2 d-flex flex-column">
-                                <img src="/img/coins-2.png" alt="Coin" class="mb-2 m-auto" style="width: 100px;>
+                                <img src="/img/coins-2.png" alt="Coin" class="mb-2 m-auto" style="width: 100px;">
                                 <h6>550コイン</h6>
                                 <p class="text-muted small mb-2" style="font-size:12px;">無料コイン150枚付き</p>
                                 <button id="coins-set2"  class="buycoin btn custom-gradient-bg w-100">¥800.0</button>
@@ -223,7 +223,7 @@ window.showCoinShop = function(){
                     <div class="col-6 mb-3">
                         <div class="card bg-dark text-white">
                             <div class="card-body p-2 d-flex flex-column">
-                                <img src="/img/coins-3.png" alt="Coin" class="mb-2 m-auto" style="width: 100px;>
+                                <img src="/img/coins-3.png" alt="Coin" class="mb-2 m-auto" style="width: 100px;">
                                 <h6>1200コイン</h6>
                                 <p class="text-muted small mb-2" style="font-size:12px;">無料コイン450枚付き</p>
                                 <button id="coins-set3"  class="buycoin btn custom-gradient-bg w-100">¥1500.0</button>
@@ -233,7 +233,7 @@ window.showCoinShop = function(){
                     <div class="col-6 mb-3">
                         <div class="card bg-dark text-white">
                             <div class="card-body p-2 d-flex flex-column">
-                                <img src="/img/coins-4.png" alt="Coin" class="mb-2 m-auto" style="width: 100px;>
+                                <img src="/img/coins-4.png" alt="Coin" class="mb-2 m-auto" style="width: 100px;">
                                 <h6>2500コイン</h6>
                                 <p class="text-muted small mb-2" style="font-size:12px;">無料コイン1000枚付き</p>
                                 <button id="coins-set4"  class="buycoin btn custom-gradient-bg w-100">¥3000.0</button>

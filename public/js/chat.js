f948fe56500fcbaf35d02606306d51cb5c86bf30
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2024-09-13 13:14:18 +0900

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -1281,7 +1281,7 @@ $(document).ready(async function() {
                         <div id="container-${designStep}">
                             <div class="d-flex flex-row justify-content-start mb-4 message-container ${isBlur ? 'unlock-nsfw':''}">
                                 <img src="${thumbnail || 'https://lamix.hatoltd.com/img/logo.webp'}" alt="avatar 1" class="rounded-circle chatbot-image-chat" data-id="${chatId}" style="min-width: 45px; width: 45px; height: 45px; border-radius: 15%; object-fit: cover; object-position: top;">
-                                <div class="position-relative">
+                                <div class="ms-3 position-relative">
                                     <div class="ps-3 text-start assistant-image-box">
                                         <img id="image-${imageId}" data-id="${imageId}" src="${response.imageUrl}" alt="${response.imagePrompt}">
                                     </div>
@@ -1825,7 +1825,7 @@ $(document).ready(async function() {
             messageElement = $(`
                 <div class="d-flex flex-row justify-content-start mb-4 message-container ${messageClass} ${animationClass}">
                     <img src="${thumbnail || 'https://lamix.hatoltd.com/img/logo.webp'}" alt="avatar" class="rounded-circle chatbot-image-chat" data-id="${chatId}" style="min-width: 45px; width: 45px; height: 45px; border-radius: 15%; object-fit: cover; object-position:top;">
-                    <div class="position-relative">
+                    <div class="ms-3 position-relative">
                         <div class="ps-3 text-start assistant-image-box">
                             ${message.outerHTML}
                         </div>

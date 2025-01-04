d73f65c9f98dee01ced605d86bfad0dad14771a1
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2025-01-04 12:47:34 +0900

    dynamic port

--- server.js
+++ server.js
@@ -672,7 +672,7 @@ fastify.get('/settings', { preHandler: [fastify.authenticate] }, async (request,
 const start = async () => {
   try {
     const port = process.env.PORT || 3000;
-    fastify.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
+    fastify.listen({ port, host: '0.0.0.0' }, (err, address) => {
       if (err) {
         console.error(err);
         process.exit(1);
